2565da3f9624107c667381133a010a613e600343
pushkar-28 <kharepushkar2804@gmail.com>
2024-07-23 01:26:21 +0530

    add: /users crud

---
+++ server/src/users/users.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): Promise<User> {
+    return this.usersService.remove(+id);
+  }
+}
---
+++ server/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private database: DatabaseService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    return this.database.user.create({ data });
+  }
+  async findOne(id: number): Promise<User> {
+    return this.database.user.findUnique({ where: { id } });
+  }
+  async update(id: number, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.database.user.update({ where: { id }, data });
+  }
+  async remove(id: number): Promise<User> {
+    return this.database.user.delete({ where: { id } });
+  }
+}
