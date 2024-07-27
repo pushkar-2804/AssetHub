846d9507e132bd91af58f22af4c1b442b23deb54
Vignesh M <vickyaadhi254@gmail.com>
2024-07-27 23:07:49 +0530

    Add: Dashboard template

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,12 +2,15 @@
     import { createEventDispatcher } from 'svelte';
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store';
+    import Footer from "$lib/Footer.svelte";
 
     const errorMessage = writable('');
     const isAuthenticated = writable(false);
+    const userMail = writable('');
 
     let token;
     let dispatch = createEventDispatcher();
+    let data;
 
     function checkAuthToken() {
     if (typeof window !== 'undefined') {
@@ -27,33 +30,60 @@
 
     if (!token) {
       isAuthenticated.set(false);
-      return;
+      // return;
     }
     else {
         isAuthenticated.set(true);
-        return;
+        // return;
     }
 
-    // try {
-    //   const response = await fetch('http://localhost:3000/api/user/profile', {
-    //     method: 'GET',
-    //     headers: {
-    //       'Authorization': `Bearer ${token}`,
-    //       'Content-Type': 'application/json'
-    //     }
-    //   });
+    try {
+      const response = await fetch('http://localhost:3000/auth/profile', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
 
-    //   if (response.ok) {
-    //     const data = await response.json();
-    //     isAuthenticated.set(true);
-    //   } else {
-    //     isAuthenticated.set(false);
-    //     errorMessage.set('Failed to fetch user details. Please log in again.');
-    //   }
-    // } catch (error) {
-    //   isAuthenticated.set(false);
-    //   errorMessage.set('An error occurred. Please try again.');
-    // }
+      if (response.ok) {
+        data = await response.json();
+        console.log(data);
+        // isAuthenticated.set(true);
+      } else {
+        // isAuthenticated.set(false);
+        errorMessage.set('Failed to fetch user details. Please log in again.');
+      }
+    } catch (error) {
+      // isAuthenticated.set(false);
+      errorMessage.set('An error occurred. Please try again.');
+    }
+
+    try{
+      const response = await fetch('http://localhost:3000/users/profile', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          userId: data.userId
+        })
+      });
+      if (response.ok){
+        const userMailData = await response.json();
+        console.log(userMail);
+        userMail.set(userMailData.email);
+        // isAuthenticated.set(true);
+      } else {
+        // isAuthenticated.set(false);
+        errorMessage.set('Failed to fetch user details. Please log in again.');
+      }
+    } catch (error) {
+      // isAuthenticated.set(false);
+      errorMessage.set('An error occurred. Please try again.');
+    }
+
 
   }
 
@@ -71,7 +101,7 @@
     {#if !$isAuthenticated}
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <form action="/login">
-            <button type="submit" class="submit-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+            <button type="submit" class="submit-button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started for free</button>
         </form>
     </div>
     {:else}
@@ -86,12 +116,14 @@
         </span>
       <div class="hidden dropdown-menu">
         <div class="absolute right-0 w-56 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none" aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+          {#if $isAuthenticated}
           <div class="px-4 py-3">
             <p class="text-sm leading-5">Signed in as</p>
-            <p class="text-sm font-medium leading-5 text-gray-900 truncate">tom@example.com</p>
+            <p class="text-sm font-medium leading-5 text-gray-900 truncate">{$userMail}</p>
           </div>
+          {/if}
           <div class="py-1">
-            <a href="#" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Dashboard</a>
+            <a href="/dashboard" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Dashboard</a>
             <a href="#" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left"  role="menuitem" >Wallet connection</a>
             <!-- <span role="menuitem" tabindex="-1" class="flex justify-between w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 cursor-not-allowed opacity-50" aria-disabled="true">New feature (soon)</span> -->
             <a href="#" class="text-gray-700 flex justify-between w-full px-4 py-2 text-sm leading-5 text-left" role="menuitem" >Account settings</a></div>
@@ -124,6 +156,29 @@
     </div>
     </div>
   </nav>
+
+  <section class="py-24 flex items-center justify-center bg-white">
+    <div class="mx-auto max-w-[43rem]">
+      <div class="text-center">
+        <!-- <p class="text-lg font-medium leading-8 text-indigo-600/95">Introducing an Asset Marketplace</p> -->
+        <h1 class="mt-3 text-[3.5rem] font-bold leading-[4rem] tracking-tight text-black">Browse, Buy and Sell Assets with Crypto</h1>
+        <p class="mt-3 text-lg leading-relaxed text-slate-400">Welcome to AssetHub, your secure online marketplace for buying and selling assets using cryptocurrency seamlessly bridging traditional and digital markets.</p>
+      </div>
+  
+      {#if !$isAuthenticated}
+      <div class="mt-6 flex items-center justify-center gap-4">
+        <a href="/login" class="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Click here to Login</a>
+      </div>
+      {:else}
+      <div class="mt-6 flex items-center justify-center gap-4">
+        <a href="#" class="transform rounded-md bg-indigo-600/95 px-5 py-3 font-medium text-white transition-colors hover:bg-indigo-700">Browse Assets</a>
+        <a href="#" class="transform rounded-md border border-slate-200 px-7 py-3 font-medium text-slate-900 transition-colors hover:bg-slate-50"> List Assets </a>
+      </div>
+      {/if}
+    </div>
+  </section>
+
+<Footer/>
   
 
   <style>
---
+++ client/src/routes/dashboard/+page.svelte
@@ -0,0 +1,136 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+    function logout() {
+      // Remove JWT token from local storage
+      localStorage.removeItem('token');
+  
+      // Redirect to the home page
+      window.location.href = '/';
+      dispatch('logout');
+    }
+  </script>
+
+
+
+<div class="flex flex-row">
+<aside class="min-h-screen bg-gray-100">
+    <div class="sidebar min-h-screen overflow-hidden border-r w-56 hover:bg-white hover:shadow-lg">
+      <div class="flex h-screen flex-col justify-between pt-2 pb-6">
+        <div>
+            <div class="w-max p-3">
+                <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="./AssetHub.svg" class="h-8" alt="AssetHub Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">AssetHub</span>
+                </a>
+              </div>
+          <ul class="mt-6 space-y-2 tracking-wide">
+            <li class="min-w-max">
+              <a href="#" aria-label="dashboard" class="relative flex items-center space-x-4 bg-gradient-to-r from-blue-700 to-sky-600 px-4 py-3 text-white">
+                <span class="-mr-1 font-medium">Dashboard</span>
+              </a>
+            </li>
+            <li class="min-w-max">
+              <a href="#" class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
+                  <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+                </svg>
+                <span class="group-hover:text-gray-700">Browse Assets</span>
+              </a>
+            </li>
+            <li class="min-w-max">
+              <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
+                  <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+                </svg>
+                <span class="group-hover:text-gray-700">List Asset</span>
+              </a>
+            </li>
+            <li class="min-w-max">
+                <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
+                    <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
+                  </svg>
+                  <span class="group-hover:text-gray-700">Wallet Connection</span>
+                </a>
+              </li>
+            <li class="min-w-max">
+              <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                  <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
+                  <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
+                </svg>
+                <span class="group-hover:text-gray-700">Profile Settings</span>
+              </a>
+            </li>
+           
+          </ul>
+        </div>
+        <div class="w-max -mb-3">
+          <button on:click={logout} class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+            <img src="./logout.svg" class="h-5 w-5 group-hover:fill-cyan-600" alt="logout" />
+            <span class="group-hover:text-gray-700">Sign out</span>
+          </button>
+        </div>
+      </div>
+    </div>
+</aside>
+
+
+
+
+
+
+
+
+ <div class="flex flex-col w-full bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex">
+      <div class="flex flex-row w-full font-medium justify-end p-6 border border-gray-100 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+        <div class="py-2 px-3">
+          <a href="#" class="mx-1.5 py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
+        </div>
+        <div class="py-2 px-3">
+          <a href="#" class="mx-1.5 py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Asset Listing</a>
+        </div>
+        <div class="py-2 px-3">
+          <a href="#" class="mx-1.5 py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Browse Assets</a>
+        </div>
+        <div class="py-2 px-3">
+          <a href="#" class="mx-1.5 py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">View Cart</a>
+        </div>
+    </div>
+    </div>
+
+
+    <div class="flex flex-wrap w-full h-full bg-gradient-to-r from-blue-700 to-sky-600">
+        <div class="py-2 px-3 w-full h-1/2">
+            <div class="flex justify-center w-full h-full rounded-lg bg-slate-200">
+                <div class="flex justify-center w-full h-10 bg-gray-100">
+                    <h1>Your Profile</h1>
+                </div>
+            </div>
+        </div>
+        <div class="py-2 px-3 w-1/2 h-1/2">
+            <div class="flex justify-center w-full h-full rounded-lg bg-slate-200">
+                <div class="flex justify-center w-full h-10 bg-gray-100">
+                    <h1>Your Cart Items</h1>
+                </div>
+            </div>
+        </div>
+        <div class="py-2 px-3 w-1/2 h-1/2">
+            <div class="flex justify-center w-full h-full rounded-lg bg-slate-200">
+                <div class="flex justify-center w-full h-10 bg-gray-100">
+                    <h1>Your Asset Listing</h1>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+ </div>
+</div>
