38cb3a404972c7c720c0ebb39e0068703d92c25e
pushkar-28 <kharepushkar2804@gmail.com>
2024-07-31 04:02:21 +0530

    add: connect wallet api

--- server/src/wallet/wallet.controller.ts
+++ server/src/wallet/wallet.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { CreateWalletDto } from './create-wallet.dto';
+import { LoginDto } from 'src/auth/dto/login.dto';
 
 @Controller('wallet')
 export class WalletController {
@@ -10,7 +17,31 @@ export class WalletController {
   @UseGuards(JwtAuthGuard)
   @Post('get-wallet-list')
   @ApiOperation({ summary: 'Log in to fetch wallet address' })
-  async login(@Body() credentials: { email: string; password: string }) {
+  @ApiResponse({
+    status: 200,
+    description: 'Wallet address fetched successfully.',
+  })
+  @ApiBearerAuth()
+  async login(@Body() credentials: LoginDto) {
     return this.walletService.login(credentials.email, credentials.password);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put('connect-wallet')
+  @ApiOperation({ summary: 'Upsert wallet address' })
+  @ApiResponse({
+    status: 200,
+    description: 'Wallet address upserted successfully.',
+  })
+  @ApiBearerAuth()
+  async upsertWalletAddress(
+    @Req() req,
+    @Body() updateWalletDto: CreateWalletDto,
+  ) {
+    const userId = req.user.userId;
+    return this.walletService.connectWallet(
+      userId,
+      updateWalletDto.walletAddress,
+    );
   }
 }
--- server/src/wallet/wallet.service.ts
+++ server/src/wallet/wallet.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 import { getWalletList } from 'src/utils/neucron-wallet.util';
 
 @Injectable()
 export class WalletService {
+  constructor(private databaseService: DatabaseService) {}
   async login(
     email: string,
     password: string,
@@ -10,6 +13,20 @@ export class WalletService {
     try {
       const wallets = await getWalletList(email, password);
       return wallets;
+    } catch (error) {
+      console.log(error);
+      throw new Error('bla');
+    }
+  }
+
+  async connectWallet(userId: number, walletAddress: string) {
+    try {
+      const wallet = await this.databaseService.wallet.upsert({
+        where: { userId },
+        update: { walletAddress },
+        create: { userId, walletAddress },
+      });
+      return wallet;
     } catch (error) {
       console.log(error);
       throw new Error('bla');
