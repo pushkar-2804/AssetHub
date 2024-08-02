9b1c65a932335c983febe5ebf46003e7dcc791fb
Yunus <tijaniyunus07@gmail.com>
2024-08-02 12:56:35 +0200

    Added My-Assets

--- client/src/routes/asset-browsing/+page.svelte
+++ client/src/routes/asset-browsing/+page.svelte
@@ -197,6 +197,9 @@
           <a href="/asset-browsing" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Browse Assets</a>
         </li>
         <li>
+          <a href="/my-assets" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">My Assets</a>
+        </li>
+        <li>
           <a href="/cart" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">View Cart</a>
         </li>
       </ul>
--- client/src/routes/dashboard/+page.svelte
+++ client/src/routes/dashboard/+page.svelte
@@ -145,7 +145,7 @@ onMount(() => {
             </a>
           </li>
           <li class="min-w-max">
-            <a href="#" class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
+            <a href="/asset-browsing" class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
                 <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
@@ -154,7 +154,16 @@ onMount(() => {
             </a>
           </li>
           <li class="min-w-max">
-            <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
+            <a href="/my-assets" class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
+                <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
+              </svg>
+              <span class="group-hover:text-gray-700">My Assets</span>
+            </a>
+          </li>
+          <li class="min-w-max">
+            <a href="/asset-listing" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
                 <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
