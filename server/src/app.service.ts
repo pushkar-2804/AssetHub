3df860230026fe1cc3dac294cc5506df13eed076
pushkar-28 <kharepushkar2804@gmail.com>
2024-08-05 00:25:18 +0530

    fix: deploy issue-5

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! from Todo rest API!';
+    return 'Hello! from Assethub rest API!';
   }
 }
